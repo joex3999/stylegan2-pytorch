b7078a77d6e5782e76c3c3e30421067a08c0c2db
Youssef Farag <farag@twaice.com>
2022-07-09 20:51:21 +0200

    fix: change data location

--- slurm.sh
+++ slurm.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name="Inf"
-#SBATCH --nodes=1
+#SBATCH --job-name="stylegan_cityscapes"
+#SBATCH --nodes=2
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=32G
 #SBATCH --gpus=a40:1
 #SBATCH --time=15:00:00
-#SBATCH --mail-type=NONE
+#SBATCH --mail-type=ALL
 ##SBATCH --partition=DEADLINE
 ##SBATCH --comment=ECCVRebuttal
 #SBATCH --output=./slurmDebug/slurm-%j.out
 #SBATCH --error=./slurmDebug/slurm-%j.out
 
 
-python main_train.py
+python -m torch.distributed.launch  train.py  "~/storage/user/main/faragy/data" --iter 1000
 #python main_test.py
