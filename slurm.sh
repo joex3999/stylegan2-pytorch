1d80b07f25853daf42f6249cda9d3ab488bec3b0
Youssef Farag <farag@twaice.com>
2022-07-12 20:20:27 +0200

    fix: modify slurm.sh

--- slurm.sh
+++ slurm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name="stylegan_cityscapes_faragy"
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=32G
 #SBATCH --gpus=a40:1
@@ -8,8 +8,7 @@
 #SBATCH --mail-type=ALL
 ##SBATCH --partition=DEADLINE
 ##SBATCH --comment=ECCVRebuttal
-#SBATCH --output=/usr/stud/faragy/storage/slurm/slurmDebug/slurm-%j.out
-#SBATCH --error=/usr/stud/faragy/storage/slurm/slurmDebug/slurm-%j.out
+#SBATCH --output=/usr/stud/faragy/storage/slurm/slurmDebug/slurm_temp_output.out
+#SBATCH --error=/usr/stud/faragy/storage/slurm/slurmDebug/slurm_temp_error.out
 
-
-python train.py  "/usr/stud/faragy/storage/user/main/faragy/data" --iter 1000
+ python train.py  "/usr/stud/faragy/storage/user/main/faragy/data" --iter 1000
