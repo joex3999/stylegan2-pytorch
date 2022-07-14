f3d330d9e8430f94a0332cd1d12a8646cfadebab
Youssef Farag <farag@twaice.com>
2022-07-14 16:18:29 +0200

    fix: add checkpoint call to slurm

--- slurm.sh
+++ slurm.sh
@@ -4,11 +4,11 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=32G
 #SBATCH --gpus=a40:1
-#SBATCH --time=15:00:00
+#SBATCH --time=20:00:00
 #SBATCH --mail-type=ALL
 ##SBATCH --partition=DEADLINE
 ##SBATCH --comment=ECCVRebuttal
 #SBATCH --output=/usr/stud/faragy/storage/slurm/slurmDebug/slurm_temp_output.out
 #SBATCH --error=/usr/stud/faragy/storage/slurm/slurmDebug/slurm_temp_error.out
 
- python train.py  "/usr/stud/faragy/storage/user/main/faragy/data" --iter 1000
+python train.py --ckpt "/usr/stud/faragy/storage/user/main/faragy/stylegan2-pytorch/checkpoint/040000.pt" "/usr/stud/faragy/storage/user/main/faragy/data" 
